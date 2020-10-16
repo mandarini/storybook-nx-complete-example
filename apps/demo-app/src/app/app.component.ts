628a5cadefba24714b3f262b0c93e64bf86a4c2d
katerina <sk.katherine@gmail.com>
2020-10-16 12:30:31 +0300

    initial commit

---
+++ apps/demo-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'storybook-angular-example-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'demo-app';
+}
