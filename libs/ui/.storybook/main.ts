9df9b2aa1c8ebd14b76738d15cc240c2d0ef3775
Yann Braga <yannbf@gmail.com>
2021-01-04 19:26:17 +0100

    feat(ui): add compostion with ui-react storybook

--- libs/ui/.storybook/main.ts
+++ libs/ui/.storybook/main.ts
@@ -4,4 +4,11 @@ const rootMain = require('../../../.storybook/main');
 // rootMain.addons.push('');
 rootMain.stories.push(...['../src/lib/**/*.stories.mdx', '../src/lib/**/*.stories.@(js|jsx|ts|tsx)'])
 
+rootMain.refs = {
+  'ui-react': {
+    title: "UI React Stories",
+    url: "http://localhost:4402" // For local projects, please run these Storybooks BEFORE running the main Storybook.
+  }
+}
+
 module.exports = rootMain;
